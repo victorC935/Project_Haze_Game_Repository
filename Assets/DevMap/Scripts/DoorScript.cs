abb31b078b17e240e4f1ad236cb18c3f49811db7
llwolf <donovan.davidka@gmail.com>
2018-06-18 16:43:46 +0200

    Script Replacements
    Added my old scripts and started implementing the door script.

---
+++ Assets/DevMap/Scripts/DoorScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+	public bool interacted;
+    public bool isLocked;
+    public bool isWooden;
+
+    [Tooltip("If the door does not have a key, input 0")]
+    public int KeyNumber; // In case the player finds a key.
+
+
+	void Start () {
+        interacted = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        IntChk();         // Does not let the player move the door when colliding.
+	}
+    void IntChk()
+    {
+        if (!interacted)
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+        }
+        if (interacted)
+        {
+            GetComponent<Rigidbody>().isKinematic = false;
+        }
+    }
+}
---
+++ Assets/DevMap/Scripts/TestMeleeScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMeleeScript : MonoBehaviour {
+
+    public GameObject TestWeapon;
+    public GameObject PlayerCamera;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        /* This part is only temporary. It should be replaced with a script that gets called when the player is either:
+                A-> Looking at a weapon that is on the ground
+                B-> Taking a weapon that is stuck in an enemy or an object
+                C-> Equipping a weapon from the inventory */
+        if (Input.GetKeyDown(KeyCode.O) && TestWeapon.GetComponent<MeleeWeaponScript>().isEquipped == false) {
+            TestWeapon.GetComponent<MeleeWeaponScript>().EquipWeapon();
+        }
+        if (Input.GetKeyDown(KeyCode.O) && TestWeapon.GetComponent<MeleeWeaponScript>().isEquipped == true)
+        {
+            TestWeapon.GetComponent<MeleeWeaponScript>().UnEquipWeapon();
+        }
+
+    }
+    void Attack()
+    {
+        // Play the animation that swings the weapon
+    }
+    void Block()
+    {
+        // Play the animation that raises the weapon to block enemy attacks from front.
+        // Check if the player is looking at the enemy, then NEGATE or DECREASE the damage taken.
+    }
+}
