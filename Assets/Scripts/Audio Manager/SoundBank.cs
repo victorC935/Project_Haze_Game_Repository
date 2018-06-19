eb6ed71ae8dbaa9ba5f3a83352e135dbfc984678
Jay <1010100@student.sae.edu.au>
2018-06-19 20:17:50 +1000

    SoundBack creation
    can/ is used for Creating Event instances for FMOD audio from the inspector into a list or via script

--- Assets/Scripts/Audio Manager/SoundBank.cs
+++ Assets/Scripts/Audio Manager/SoundBank.cs
@@ -1,16 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMOD.Studio;
+using FMODUnity;
 
-public class SoundBank : MonoBehaviour {
+using FMODhotfixes;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class SoundBank : MonoBehaviour
+{
+    //FMODUnity Event references
+    //This is for allocating sound events in the editor to allow designers to implement the sound system
+    public List<EventInstanceWrapper> MasterEventBank;
+
+    #region Example code
+    /// <summary>
+    /// Used to give example of how to create an Instance of an FMOD event  and play / stop it.
+    /// use the wrapper class if needed.
+    /// </summary>
+    private void ExampleEventInstance()
+    {
+
+        EventInstanceWrapper exampleMusicEvent;
+        //exampleMusicEvent.EventInstance.start();
+        //exampleMusicEvent.EventInstance.stop(STOP_MODE.IMMEDIATE);
+    }
+
+    ///Could be used to set and validate event instances if done by loading GUID's and references via script
+    //private void OnValidate()
+    //{
+    //    foreach (FMOD.hotfix.FMODEventWrapper arg0 in MasterEventBank)
+    //    {
+    //        Debug.Log("event instance triggered");
+    //        arg0.SetInstanceTest();
+    //    }
+    //}
+
+    #endregion
+
 }
