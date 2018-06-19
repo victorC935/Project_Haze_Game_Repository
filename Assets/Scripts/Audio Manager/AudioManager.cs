23be66e7d8ac12b4f48d7902f4a23f43f48f502f
Jay <1010100@student.sae.edu.au>
2018-06-19 20:26:38 +1000

    AudioManager singleton

--- Assets/Scripts/Audio Manager/AudioManager.cs
+++ Assets/Scripts/Audio Manager/AudioManager.cs
@@ -1,16 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//Apparently caused some namespace issues with system when using. not necessary anyway
+//using FMOD;
+using FMOD.Studio;
+using FMODUnity;
 
-public class AudioManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private static AudioManager _instance;
+    public static AudioManager Instance
+    {
+        get 
+        {
+            return _instance;
+        }
+
+        set
+        {
+            _instance = value;
+        }
+    }
+
+    public void Awake()
+    {
+        #region Duplicate instance check
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        #endregion
+    }
 }
