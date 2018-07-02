ed60ec7f7ca02c14fec818674b51571eadec3b01
llwolf <donovan.davidka@gmail.com>
2018-07-02 10:54:11 +0200

    Objects using hinges

--- Assets/Scripts/InteractionScript.cs
+++ Assets/Scripts/InteractionScript.cs
@@ -12,27 +12,29 @@ public class InteractionScript : MonoBehaviour
     GameObject hitObject;
     public GameObject CurrentWeapon;
 
-    public GameObject lastDoor;
+    public GameObject lastObject;
 
+    public bool canSeeSomething;
 
     [Range(5.0f, 10.0f)] [Header("Recommended value is 8")]
     public float doorSpeed;
 
-    private float openSpeed;
+    private float openSpeedHorizontal;
+    private float openSpeedVertical;
 
 
 
     void Start()
     {
         CurrentWeapon = null;
-        lastDoor = null;
+        lastObject = null;
     }
 
     // Update is called once per frame
     void Update()
     {
-        openSpeed = Input.GetAxis("Mouse X");
-        openSpeed = Mathf.Clamp(openSpeed, -0.5f, 0.5f);
+        openSpeedHorizontal = Input.GetAxis("Mouse X");
+        openSpeedHorizontal = Mathf.Clamp(openSpeedHorizontal, -0.5f, 0.5f);
 
         RaycastHit hit;
         if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 2.5f, layerMask))
@@ -54,34 +56,50 @@ public class InteractionScript : MonoBehaviour
                 }
             }
         }
-        RaycastHit doorHit;
-        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out doorHit, 2.5f, interactableLayerMask))
+        RaycastHit objHit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out objHit, 2.5f, interactableLayerMask))
         {
+            canSeeSomething = true;
             if (Input.GetButtonDown("Use")) // This is to be used for opening the door or starting the lockpicking sequence if the player has the lockpick selected.
             {
-                hitObject = doorHit.collider.gameObject;
+                hitObject = objHit.collider.gameObject;
 
-                if(hitObject.tag == "Door") // Check if the object has the tag Door, so it can actually access the script.
+                if(hitObject.tag == "HingeObject") // Check if the object has the tag Door, so it can actually access the script.
                 {
-                    lastDoor = hitObject;
+                    lastObject = hitObject.transform.parent.gameObject;
                 }
             }
         }
-        if (Input.GetButtonUp("Use") && lastDoor != null)
+        if (!Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out objHit, 2.5f, interactableLayerMask))
         {
-            lastDoor.GetComponent<DoorScript>().interacted = false; // Lock the door in place.
-            lastDoor = null;
+            canSeeSomething = false;        // This helps, so the player does not open doors they are not facing at.
+
         }
-        if (lastDoor != null) // Checks if the door is not locked
+        #region Door Related
+        if (Input.GetButtonUp("Use") && lastObject != null && lastObject.GetComponent<ObjectHingeScript>().isDoor)
         {
-            if (!lastDoor.GetComponent<DoorScript>().isLocked && Input.GetButton("Use"))
+            lastObject.GetComponent<ObjectHingeScript>().interacted = false; // Lock the door in place.
+            lastObject = null;
+        }
+        if (lastObject != null) // Checks if the door is not locked
+        {
+            if (!lastObject.GetComponent<ObjectHingeScript>().isLocked && Input.GetButton("Use"))
             {
+                float distanceFromDoorknob = Vector3.Distance(gameObject.transform.position, lastObject.GetComponent<ObjectHingeScript>().doorKnob.transform.position);
+                Debug.Log(distanceFromDoorknob);
                 // This part will translate horizontal mouse movement, to apply forces on the door's Rigidbody component so it will actually open.
-                lastDoor.GetComponent<DoorScript>().interacted = true;
-
-                lastDoor.GetComponent<Rigidbody>().AddForce(-openSpeed *(doorSpeed * 100), 0, openSpeed *(doorSpeed * 100));    // applies force to open the door
-
+                lastObject.GetComponent<ObjectHingeScript>().interacted = true;
+                if (hitObject.name == "OpenFrom" && canSeeSomething)
+                {
+                    lastObject.GetComponent<Rigidbody>().AddForce(-openSpeedHorizontal * (doorSpeed * 100), 0, openSpeedHorizontal * (doorSpeed * 100));    // applies force to open the door
+                }
+                if (hitObject.name == "OpenTo" && canSeeSomething)
+                {
+                    lastObject.GetComponent<Rigidbody>().AddForce(openSpeedHorizontal * (doorSpeed * 100), 0, -openSpeedHorizontal * (doorSpeed * 100));    // applies force to open the door
+                }
             }
         }
+        // TO DO: Get the camera velocity and use it with the door opening function to make the door opening more natural
+        #endregion
     }
 }
--- Assets/Scripts/ObjectHingeScript.cs
+++ Assets/Scripts/ObjectHingeScript.cs
@@ -2,24 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorScript : MonoBehaviour {
+public class ObjectHingeScript : MonoBehaviour {
 
-	public bool interacted;
-    public bool isLocked;
+    // Variables that are only used by the Door game object.
+    #region DoorVariables   
     public bool isWooden;
+
+    public GameObject doorKnob;
+    #endregion
+    // Bool variables to determine the object type.
+    #region ObjectTypes     
+    public bool isDoor;
+    public bool isChest;
+    #endregion
+    // Shared variables.
+    #region Shared
+    public bool interacted;
+    public bool isLocked;
+    #endregion
 
     [Tooltip("If the door does not have a key, input 0")]
     public int KeyNumber; // In case the player finds a key.
 
 
-	void Start () {
+    void Start()
+    {
         interacted = false;
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update()
+    {
         IntChk();         // Does not let the player move the door when colliding.
-	}
+    }
     void IntChk()
     {
         if (!interacted)
